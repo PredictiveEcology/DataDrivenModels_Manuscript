7e4a366516386200812ec51b9b1df708ece201ac
Ian Eddy <ianmseddy@gmail.com>
2025-06-23 11:35:54 -0700

    updated workflow

--- global.R
+++ global.R
@@ -11,6 +11,11 @@ customOpts <- list(gargle_oauth_email = "ianmseddy@gmail.com",
              reproducible.inputPaths = "~/data"
              )
 #usethis failed to install first time around...not sure why - 
+
+studyAreaEcozone <- "Boreal Shield"
+#can also be Montane Cordillera or Boreal PLains - note the typo
+
+
 inSim <- SpaDES.project::setupProject(
   packages = c("usethis", "googledrive", "httr2", "RCurl", "XML", "bcdata"),
   useGit=  TRUE,
@@ -21,54 +26,48 @@ inSim <- SpaDES.project::setupProject(
                inputPath = file.path("inputs"),
                outputPath = file.path("outputs")
   ),
-  modules = c("PredictiveEcology/Biomass_speciesFactorial",
-              "PredictiveEcology/Biomass_speciesParameters",
-              "PredictiveEcology/Biomass_core"),
+  modules = c("PredictiveEcology/Biomass_speciesFactorial@development"
+              # , "PredictiveEcology/Biomass_speciesParameters"
+              # , "PredictiveEcology/Biomass_core"
+              ),
   options = customOpts, 
   times = list(start = 2011, end = 2021),
-  #also canClimateData must come before fireSense
-  # studyArea = {
-  #   sa <- bcdata::
-  #   sa <- sa[sa$FRU == FRU,]
-  #   sa <- terra::vect(sa)
-  #   sa <- terra::buffer(sa, 3000)
-  #   sa <- terra::fillHoles(sa)
-  # },
-  # rasterToMatch = {
-  #   rtm <- terra::rast(sa, res = c(250, 250))
-  #   rtm[] <- 1
-  #   rtm <- reproducible::postProcess(rtm, maskTo = sa)
-  # },
-  # rasterToMatch_biomassParam = rasterToMatch,
-  # studyArea_biomassParam = studyArea,
-  # sppEquiv = {
-  #   spp <- LandR::speciesInStudyArea(studyArea = studyArea,
-  #                                    dPath = "inputs",)
-  #   sppEquiv <- LandR::sppEquivalencies_CA[KNN %in% spp$speciesList,]
-  #   sppEquiv <- sppEquiv[LANDIS_traits != "",]
-  #   sppEquiv <- sppEquiv[grep(pattern = "Spp", x = sppEquiv$KNN, invert = TRUE),]
-  # },
-  # objectSynonyms = list(c("rstLCC","rstLCC2011"),
-  #                       # c("flammableRTM", "flammableRTM2011"),
-  #                       # c("nonForest_timeSinceDisturbance", "nonForest_timeSinceDisturbance2011"),
-  #                       # c("landcoverDT", "landcoverDT2011"),
-  #                       c("standAgeMap", "standAgeMap2011")
-  # ),
-  params = list(
-    .globals = list(.studyAreaName = paste0("FRU", FRU),
-                    dataYear = 2011,
-                    .plots = "png",
-                    .useCache = FALSE,
-                    sppEquivCol = "LandR"),
-    Biomass_borealDataPrep = list(
-      overrideAgeInFires = FALSE,
-      overrideBiomassInFires = FALSE,
-      .useCache = c(".inputObjects")
-    ),
-    fireSense_dataPrepFit = list(
-      igAggFactor = 4),
-    canClimateData = list(
-      ".useCache" = c(".inputObjects"))
-    # fireSense_IgnitionFit = list(".plots" = )
-  )
+  studyArea = {
+    sa <- reproducible::prepInputs(url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/region/ecoregion_shp.zip", 
+                                   destinationPath = paths$inputPath, 
+                                   fun = "terra::vect"
+                                   )
+    targetCRS <- terra::crs("EPSG:3348")
+    # EPSG:3348 (NAD83(CSRS) / Statistics Canada Lambert) are commonly used for large areas of Canada. 
+    # c("Clear Hills Upland") Boreal Plains - Ecoregion 137
+    # c("Fraser Plateau") Montane Cordillera Ecoregion 202
+    # c("Lake of the Woods") Boreal Shield Ecoregion 91
+    ecodistrictName <- switch(studyAreaEcozone, 
+                              "Boreal Shield" = {"Lake of the Woods"},
+                              "Montane Cordillera" = {"Fraser Plateau"},
+                              "Boreal PLains" = {"Clear Hills Upland"})
+    sa <- sa[sa$REGION_NAM == ecodistrictName,] |>
+      terra::project(targetCRS) 
+    #consider buffering? not sure if we have disturbances yet
+    #sa <- terra::buffer(sa, 5000)
+    return(sa)
+  },  
+  rasterToMatch = {
+    rtm <- terra::rast(sa, res = c(250, 250), vals = 1) |>
+    reproducible::postProcess(maskTo = sa)
+  },
+  studyAreaANPP = {
+    ecozones <- reproducible::prepInputs(url = "https://sis.agr.gc.ca/cansis/nsdb/ecostrat/zone/ecozone_shp.zip", 
+                            destinationPath = paths$inputPath)
+    ecozones <- ecozones[ecozones$ZONE_NAME == studyAreaEcozone]
+  },
+  sppEquiv = {
+    spp <- LandR::speciesInStudyArea(studyArea = studyArea,
+                                     dPath = "inputs",)
+    sppEquiv <- LandR::sppEquivalencies_CA[KNN %in% spp$speciesList,]
+    sppEquiv <- sppEquiv[LANDIS_traits != "",]
+    sppEquiv <- sppEquiv[grep(pattern = "Spp", x = sppEquiv$KNN, invert = TRUE),]
+  }
 )
+# 8: FADM - Timber Supply Area (TSA) (multiple, wms, kml, oracle_sde)
+# ID: 8daa29da-d7f4-401c-83ae-d962e3a28980
